3b1311c8c49478aef78c9db89fc1179fe8119209
WU,BING-YING <76834811+kos8123@users.noreply.github.com>
2025-03-04 20:39:13 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://admin:Aa0909263940!@34.207.112.153:3306/cxcxc_DB")
+engine = create_engine("mysql+pymysql://root:Aa0909263940!@34.207.112.153:3306/cxcxc_DB")
 SessionLocal = sessionmaker(bind=engine)
 database = declarative_base()
 
