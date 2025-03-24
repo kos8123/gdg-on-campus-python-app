f74222ea5f93e05457d242d0da38a33b44c7aad5
WU,BING-YING <76834811+kos8123@users.noreply.github.com>
2025-03-24 15:53:32 +0800

    Update cookie.sql

--- cookie.sql
+++ cookie.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `cxcxc_DB`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `cxcxc_DB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `cxcxc_DB`;
+
+--
 -- Table structure for table `user`
 --
 
@@ -50,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-21  2:04:53
+-- Dump completed on 2025-03-24  7:50:51
